96ee85b1d4f5c8d1b5a1543804b0230eba1c94eb
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-23 18:27:57 +0300

    Model Private Post

--- tweets/models.py
+++ tweets/models.py
@@ -9,8 +9,22 @@ class BlogPost(models.Model):
     def __str__(self):
         """String representation"""
         text_len = len(self.text[:])
-        if text_len <= 50:
+        if text_len <= 200:
             return f'{self.text[:]}'
         else:
-            return f'{self.text[:50]}...'
+            return f'{self.text[:200]}...'
 
+
+class PrivatePost(models.Model):
+    """Post seen only by registered users"""
+    title = models.CharField(max_length=150)
+    text = models.TextField
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """String representation"""
+        text_len = len(self.text[:])
+        if text_len <= 200:
+            return f'{self.text[:]}'
+        else:
+            return f'{self.text[:200]}...'
+        
