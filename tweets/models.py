ca38fd8d1e6610326669a625200cc936c6ab22ec
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-23 18:12:37 +0300

    Model BlogPost

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    """One post in the blog"""
+    title = models.CharField(max_length=150)
+    text = models.TextField(max_length=1000)
+    date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """String representation"""
+        text_len = len(self.text[:])
+        if text_len <= 50:
+            return f'{self.text[:]}'
+        else:
+            return f'{self.text[:50]}...'
+
